01c2d12274a5f4bd68bb4271121cb368b1bec25b
MANI S <maniinnovator26@gmail.com>
2023-06-26 14:55:24 +0530

    day 1 1st

---
+++ basic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class LinkedList{
+    Node head;
+    class Node{
+        int data;
+        Node next; //pointer  -> will denote the address of the next node 
+        // the value next is refer the any object of node class
+    }
+}
+
+public class basic {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.close();
+    }
+}
