7449bf216ff7ed3c61d95a726fb576e2fea39c6b
MANI S <maniinnovator26@gmail.com>
2023-06-28 09:31:49 +0530

    day 3

---
+++ Reverse.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Reverse{
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        while (data != -1) {
+            list.insertAtBegin(data);
+            data = sc.nextInt(); 
+        }
+        sc.close();
+        list.display();
+    }
+}
+class LinkedList {
+    Node head;
+    class Node {
+        int data;
+        Node next;
+        Node(int val) {
+            data = val;
+            next = null;
+        }
+    }
+    LinkedList() {
+        head = null;
+    }
+    public void insertAtBegin(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
